ee4613c69768262e828a98b6bde79b065ac9e195
sgfx <860441291@qq.com>
2022-11-14 07:39:11 +0000

    add varDemo.java

---
+++ guide/varDemo.java
@@ -0,0 +1,20 @@
+public class varDemo {
+    public static void main(String [] args) {
+        // int var 
+        var intNumbre = 10;
+        System.out.println("int var demo number is :"+intNumbre);
+
+        // float var 
+        var floatNumbre = 10.0;
+        System.out.println("float var demo number is :"+floatNumbre);
+
+        // char
+        var charChar = 'A';
+        System.out.println("char var demo char is :"+charChar);
+
+        //String
+        var stringString = "Hello A";
+        System.out.println("string var demo string is :"+stringString);
+
+    }
+}
