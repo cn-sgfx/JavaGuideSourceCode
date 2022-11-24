9ea4b652cf10b3de73c3f7fb2240e7c66828d1bd
sgfx <860441291@qq.com>
2022-11-24 11:57:50 +0800

    Add files via upload

---
+++ basicGuide/arrayDemo.java
@@ -0,0 +1,32 @@
+public class arrayDemo {
+
+// 一维数组
+    public void oneDimensionalArray() {
+        // 定义数组
+        int[] anArray;
+        // 分配数组空间
+        anArray = new int[10];
+        // 初始化数组
+        int[] anArray = {
+            100,200,300,
+            400,500,600,
+            700,800,900,1000
+        }
+        System.out.println(anArray[0]);
+    }
+
+// 二维数组
+    public void twoDimensionalArray() {
+        String[][] = names = {
+            {"me","mrs.","ms."},
+            {"Smith","Jons"}
+        }
+        System.out.pritnln(names[0][0]+names[1][0]);
+        System.out.println(names[0][2]+names[1][1]);
+    }
+
+
+    public static void main(String[]args) {
+        
+    }
+}
